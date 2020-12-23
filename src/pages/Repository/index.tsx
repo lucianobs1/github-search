8686e176c6a2ac535b3c66a7e7d719c942fe919f
lucianobs1 <lucianotetsu@gmail.com>
2020-12-23 02:59:03 -0300

    config eslint, prettier and routes in project

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repository: React.FC = () => (
+  <div>
+    <h1>Repository</h1>
+    <p>Teste</p>
+  </div>
+);
+
+export default Repository;
