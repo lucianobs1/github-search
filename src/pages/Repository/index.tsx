f98e5d8aaf1e5e16bd64ad68627b46ecf339a502
lucianobs1 <lucianotetsu@gmail.com>
2020-12-26 11:25:31 -0300

    add rules and routes between pages

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 // import { Container } from './styles';
 
-const Repository: React.FC = () => (
-  <div>
-    <h1>Repository</h1>
-    <p>Teste</p>
-  </div>
-);
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+  return <h1>Repository:{params.repository}</h1>;
+};
 
 export default Repository;
